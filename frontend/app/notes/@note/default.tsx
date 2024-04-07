1f971cf9f0c7061e8e45516876efa260f98f4b57
Juan Hamilton <hedwards507@gmail.com>
2024-04-06 21:01:56 -0500

    Load note after selection

--- frontend/app/notes/@note/default.tsx
+++ frontend/app/notes/@note/default.tsx
@@ -1,7 +1,15 @@
 'use client';
 
+import { useContext } from 'react';
 import EmptyState from './components/EmptyState';
+import { NotesContext } from '../NotesContext';
+import NotePage from './page';
 
 export default function Default() {
-  return <EmptyState />;
+  const { currentNoteId } = useContext(NotesContext);
+
+  if (!currentNoteId) {
+    return <EmptyState />;
+  }
+  return <NotePage />;
 }
